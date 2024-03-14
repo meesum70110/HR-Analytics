c151058dfa012ce329138f5f505b3c936786ea6a
meesum70110 <meesum70110@gmail.com>
2024-03-14 16:03:45 +0500

    Updates

---
+++ precache-manifest.c9b115ace7d44694732ba97979d71560.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f807468177ae4f83fc07e91b47a7e3",
+    "url": "/HR-Analytics/index.html"
+  },
+  {
+    "revision": "f075dfcec0ad64c65f8f",
+    "url": "/HR-Analytics/static/css/main.2bd279a6.chunk.css"
+  },
+  {
+    "revision": "900b87366d7daf02529b",
+    "url": "/HR-Analytics/static/js/2.6c320300.chunk.js"
+  },
+  {
+    "revision": "28bc9d147a79373c4500",
+    "url": "/HR-Analytics/static/js/3.62d5139d.chunk.js"
+  },
+  {
+    "revision": "f075dfcec0ad64c65f8f",
+    "url": "/HR-Analytics/static/js/main.3c643cc7.chunk.js"
+  },
+  {
+    "revision": "5974f40e068a69658e36",
+    "url": "/HR-Analytics/static/js/runtime~main.74552127.js"
+  },
+  {
+    "revision": "326e6a11c5171f09ac46aad3f2e48b52",
+    "url": "/HR-Analytics/static/media/Devsinc_logo.326e6a11.png"
+  }
+]);
